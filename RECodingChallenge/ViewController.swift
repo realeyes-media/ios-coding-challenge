9d4292a570540fe345edb811acc7c1eae7f62743
John G. Gainfort, Jr <john@realeyes.com>
2018-10-11 16:02:39 -0600

    Initial Commit

---
+++ RECodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RECodingChallenge
+//
+//  Created by John Gainfort on 10/11/18.
+//  Copyright © 2018 RealEyes Media, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
